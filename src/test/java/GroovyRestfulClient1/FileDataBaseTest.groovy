9b0d41fbdfe1c9154040033bdb9808ab2330b3ac
ramreuveni <ramreuveni@gmail.com>
2017-09-02 19:14:11 +0300

    First commit

--- src/test/java/GroovyRestfulClient1/FileDataBaseTest.groovy
+++ src/test/java/GroovyRestfulClient1/FileDataBaseTest.groovy
@@ -12,20 +12,33 @@ class FileDataBaseTest extends GroovyTestCase {
     @Test
     void testFlow() {
 
+        /*
+        Set initial JSON file with one FileDataBase object
+         */
+        def jsonFile = new File('C:\\Users\\REUVENI\\IdeaProjects\\GroovyTest\\src\\main\\resources\\json1.json')
+        def fileData1 = new FileDataBase( 'InitialFileName' )
+        println new JsonBuilder( fileData1 ).toPrettyString()
+        jsonFile.append(new JsonBuilder( fileData1 ).toPrettyString())
+        println(jsonFile.getText('UTF-8'))
+
+        /*
+        Get all file from directory
+         */
         def filesList = [];
         def dir = new File("D:\\Temp");
         dir.eachFileRecurse(){file ->
             filesList << file
         }
+
+        /*
+        Append each file as FileDataBase object to JSON file
+         */
         filesList.each {
-            println it.path
+            def s=(it.path).substring(8)
+            def fileDataObj = new FileDataBase(s)
+            jsonFile.append(new JsonBuilder( fileDataObj ).toPrettyString())
         }
 
-
-        def jsonFile = new File('C:\\Users\\REUVENI\\IdeaProjects\\GroovyTest\\src\\main\\resources\\json1.json')
-        //def fileData1 = new FileDataBase( fileName:  )
-        //println new JsonBuilder( fileData1 ).toPrettyString()
-
+        println(jsonFile.getText('UTF-8'))
     }
-
 }
